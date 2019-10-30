7535fc5078060395c4f9a0c333d85fa6c9d3e800
Zach Thornton <contact@zachthornton.dev>
2019-10-30 11:03:59 -0400

    Updated plugin paths

--- __init__.py
+++ __init__.py
@@ -1,12 +1,14 @@
 from airflow.plugins_manager import AirflowPlugin
 from facebook_ads_plugin.hooks.facebook_ads_hook import FacebookAdsHook
-from facebook_ads_plugin.operators.facebook_ads_to_s3_operator import FacebookAdsInsightsToS3Operator
+from facebook_ads_plugin.operators.facebook_ads_to_gcs_operator import (
+    FacebookAdsInsightsToGCSOperator,
+)
 
 
 class FacebookAdsPlugin(AirflowPlugin):
     name = "FacebookAdsPlugin"
     hooks = [FacebookAdsHook]
-    operators = [FacebookAdsInsightsToS3Operator]
+    operators = [FacebookAdsInsightsToGCSOperator]
     executors = []
     macros = []
     admin_views = []
